f736d45b7c2ebb6ca1e8533750bdf883b7665be4
madMax342 <maximgel@post.bgu.ac.il>
2021-04-28 11:44:09 +0300

    env-store update and .gitignore

--- src/ex3/L21-env-store.ts
+++ src/ex3/L21-env-store.ts
@@ -17,18 +17,21 @@ export interface Store {
     vals: Box<Value>[];
 }
 
-export const isStore = ...;
-export const makeEmptyStore = ...;
-export const theStore: Store = 
-export const extendStore = (s: Store, val: Value): Store =>
-    // Complete
-    
-export const applyStore = (store: Store, address: number): Result<Value> =>
-    // Complete
+export const isStore = (x: any): x is Store => x.tag === "Store";                       // Added this
+export const makeEmptyStore = (): Store => ({tag: "Store", vals: makeBox([])});         // Added this, may work
+export const theStore: Store = makeEmptyStore();                                        // Added this
+
+export const extendStore = (s: Store, val: Value): Store =>                             // Added this
+    ({tag: "Store", vals: s.vals.concat([makeBox(val)])});
+
+
+export const applyStore = (store: Store, address: number): Result<Value> =>             // Added this, address may be changed...
+    address < store.vals.length ? makeOk(unbox(store.vals[address])) :
+    makeFailure(`Address ${address} is larger than current values count.`);
 
     
-export const setStore = (store: Store, address: number, val: Value): void => 
-    // Complete
+export const setStore = (store: Store, address: number, val: Value): void =>            // Added this,may be changed as well
+    setBox(store.vals[address], val);
 
 
 // ========================================================
@@ -70,7 +73,7 @@ export const applyEnv = (env: Env, v: string): Result<number> =>
     applyExtEnv(env, v);
 
 const applyGlobalEnv = (env: GlobalEnv, v: string): Result<number> => 
-    // Complete
+    map((v: Box<string[]>) => unbox(v) ,env.vars) 
 
 export const globalEnvAddBinding = (v: string, addr: number): void =>
     // Complete
